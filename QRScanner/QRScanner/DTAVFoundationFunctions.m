5c75362e36f6c3b1518a2307ecb286a602aa744a
Oliver Drobnik <oliver@cocoanetics.com>
2013-12-10 16:07:36 +0100

    added helper method for getting CGPath from metadata object

--- QRScanner/QRScanner/DTAVFoundationFunctions.m
+++ QRScanner/QRScanner/DTAVFoundationFunctions.m
@@ -26,3 +26,35 @@ AVCaptureVideoOrientation DTAVCaptureVideoOrientationForUIInterfaceOrientation(U
 		}
 	}
 }
+
+CGPathRef DTAVMetadataMachineReadableCodeObjectCreatePathForCorners(AVCaptureVideoPreviewLayer *previewLayer,  AVMetadataMachineReadableCodeObject *barcodeObject)
+{
+	AVMetadataMachineReadableCodeObject *transformedObject = (AVMetadataMachineReadableCodeObject *)[previewLayer transformedMetadataObjectForMetadataObject:barcodeObject];
+	
+	// new mutable path
+	CGMutablePathRef path = CGPathCreateMutable();
+	
+	// first point
+	CGPoint point;
+	CGPointMakeWithDictionaryRepresentation((__bridge CFDictionaryRef)(transformedObject.corners[0]), &point);
+	CGPathMoveToPoint(path, NULL, point.x, point.y);
+	
+	// second point
+	CGPointMakeWithDictionaryRepresentation((__bridge CFDictionaryRef)(transformedObject.corners[1]), &point);
+	CGPathAddLineToPoint(path, NULL, point.x, point.y);
+	
+	// third point
+	CGPointMakeWithDictionaryRepresentation((__bridge CFDictionaryRef)(transformedObject.corners[2]), &point);
+	CGPathAddLineToPoint(path, NULL, point.x, point.y);
+	
+	// fourth point
+	CGPointMakeWithDictionaryRepresentation((__bridge CFDictionaryRef)(transformedObject.corners[3]), &point);
+	CGPathAddLineToPoint(path, NULL, point.x, point.y);
+	
+	// and back to first point
+	CGPathCloseSubpath(path);
+	
+	return path;
+}
+
+
