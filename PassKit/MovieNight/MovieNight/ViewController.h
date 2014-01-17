e900cb5edaa8fedbbc99e620549ce8b1ffdad527
Oliver Drobnik <oliver@cocoanetics.com>
2014-01-17 14:57:40 +0100

    Added MovieNight stup

---
+++ PassKit/MovieNight/MovieNight/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MovieNight
+//
+//  Created by Oliver Drobnik on 17.01.14.
+//  Copyright (c) 2014 Cocoanetics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
