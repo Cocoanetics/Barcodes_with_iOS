57bdb2c0bf2e265431242ab9d7b0bf97551f9999
Oliver Drobnik <oliver@cocoanetics.com>
2014-02-17 16:41:50 +0100

    Added QR generation and copying

---
+++ QRBuilder/QRBuilder/DTBarcodeImageView.h
@@ -0,0 +1,13 @@
+//
+//  DTBarcodeImageView.h
+//  QRBuilder
+//
+//  Created by Oliver Drobnik on 17.02.14.
+//  Copyright (c) 2014 Cocoanetics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DTBarcodeImageView : UIImageView
+
+@end
---
+++ QRBuilder/QRBuilder/DTBarcodeImageView.m
@@ -0,0 +1,70 @@
+//
+//  DTBarcodeImageView.m
+//  QRBuilder
+//
+//  Created by Oliver Drobnik on 17.02.14.
+//  Copyright (c) 2014 Cocoanetics. All rights reserved.
+//
+
+#import "DTBarcodeImageView.h"
+
+@implementation DTBarcodeImageView
+
+- (void)awakeFromNib {
+   UILongPressGestureRecognizer *longPress =
+   [[UILongPressGestureRecognizer alloc] initWithTarget:self
+                                                 action:@selector(handleLongPress:)];
+   [self addGestureRecognizer:longPress];
+   
+   self.userInteractionEnabled = YES;
+}
+
+- (BOOL)_hasBarcodeSet {
+   if (self.image)
+   {
+      return YES;
+   }
+   
+   return NO;
+}
+
+- (BOOL)canBecomeFirstResponder
+{
+   return [self _hasBarcodeSet];
+}
+
+- (BOOL)canPerformAction:(SEL)action withSender:(id)sender {
+   if (action == @selector(copy:))
+   {
+      return [self _hasBarcodeSet];
+   }
+   
+   return [super canPerformAction:action withSender:sender];
+}
+
+#pragma mark - Actions
+
+
+- (void)copy:(id)sender {
+   [[UIPasteboard generalPasteboard] setImage:self.image];
+}
+
+- (void)handleLongPress:(UILongPressGestureRecognizer *)gesture {
+   if (gesture.state == UIGestureRecognizerStateBegan)
+   {
+      if (![self _hasBarcodeSet])
+      {
+         return;
+      }
+      
+      [self becomeFirstResponder];
+      
+      UIMenuController *menu = [UIMenuController sharedMenuController];
+      
+      [menu setTargetRect:self.bounds inView:self];
+      [menu setArrowDirection:UIMenuControllerArrowLeft];
+      [menu setMenuVisible:YES animated:YES];
+   }
+}
+
+@end
