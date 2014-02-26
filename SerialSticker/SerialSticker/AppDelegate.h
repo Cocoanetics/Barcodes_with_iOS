5ffd8342a296efa4325c74b4221e6952b514276b
Oliver Drobnik <oliver@cocoanetics.com>
2014-02-26 14:26:53 +0100

    initial sticker app setup

---
+++ SerialSticker/SerialSticker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SerialSticker
+//
+//  Created by Oliver Drobnik on 26.02.14.
+//  Copyright (c) 2014 Cocoanetics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SerialSticker/SerialSticker/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SerialSticker
+//
+//  Created by Oliver Drobnik on 26.02.14.
+//  Copyright (c) 2014 Cocoanetics. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
