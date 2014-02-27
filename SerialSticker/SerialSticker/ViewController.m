a1879d79aba86c403b2b48b55dc97574e89f352f
Oliver Drobnik <oliver@cocoanetics.com>
2014-02-27 11:37:39 +0100

    basic Code93 generation implemented

--- SerialSticker/SerialSticker/ViewController.m
+++ SerialSticker/SerialSticker/ViewController.m
@@ -17,15 +17,64 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+   
+   self.textField.text = @"1234567890";
+   [self _updateCodeFromTextField];
 }
+
+
+- (NSUInteger)_maxBarScaleThatFitsCode:(BCKCode *)code
+{
+   CGSize maxSize = self.imageView.frame.size;
+   NSInteger retScale = 1;
+
+   for (NSUInteger scale=1;;scale++)
+   {
+      NSDictionary *options = @{BCKCodeDrawingBarScaleOption: @(scale)};
+      CGSize size = [code sizeWithRenderOptions:options];
+      
+      if (size.width > maxSize.width
+          || size.height > maxSize.height) {
+         return retScale;
+      }
+      
+      retScale = scale;
+   }
+}
+
+- (void)_updatePreviewFromCode:(BCKCode *)code
+{
+   if (!code) {
+      self.imageView.image = nil;
+      return;
+   }
+   
+   NSInteger barScale = [self _maxBarScaleThatFitsCode:code];
+   NSDictionary *options = @{BCKCodeDrawingBarScaleOption: @(barScale)};
+   UIImage *image = [UIImage imageWithBarCode:code options:options];
+   self.imageView.image = image;
+}
+
+- (void)_updateCodeFromTextField
+{
+   NSError *error;
+   BCKCode93Code *code = [[BCKCode93Code alloc] initWithContent:
+                          self.textField.text error:&error];
+   
+   if (!code) {
+      NSLog(@"%@", [error localizedDescription]);
+   }
+   
+   [self _updatePreviewFromCode:code];
+}
+
 
 - (IBAction)print:(UIButton *)sender {
    NSLog(@"Print pushed");
 }
 
 - (IBAction)textFieldChanged:(UITextField *)sender {
-   NSLog(@"New value: %@", sender.text);
+   [self _updateCodeFromTextField];
 }
 
 @end
