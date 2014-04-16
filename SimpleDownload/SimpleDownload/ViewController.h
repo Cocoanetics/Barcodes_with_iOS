5b77f0617c0cdc25e793eb6b17136f9c091985cc
Oliver Drobnik <oliver@cocoanetics.com>
2014-04-16 13:28:30 +0200

    Added SimpleDownload sample

---
+++ SimpleDownload/SimpleDownload/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  SimpleDownload
+//
+//  Created by Oliver Drobnik on 16.04.14.
+//  Copyright (c) 2014 Cocoanetics. All rights reserved.
+//
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITextField *urlField;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
