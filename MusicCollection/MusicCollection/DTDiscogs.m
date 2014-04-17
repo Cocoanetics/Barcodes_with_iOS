015849208f7882a778416dd4754dc1db1d773e23
Oliver Drobnik <oliver@cocoanetics.com>
2014-04-17 15:19:06 +0200

    adjusted formatting for book chapter

--- MusicCollection/MusicCollection/DTDiscogs.m
+++ MusicCollection/MusicCollection/DTDiscogs.m
@@ -11,16 +11,9 @@
 #define API_ENDPOINT @"http://api.discogs.com"
 #define URLENC(string) [string \
    stringByAddingPercentEncodingWithAllowedCharacters:\
-	NSCharacterSet.URLQueryAllowedCharacterSet];
-
+	[NSCharacterSet URLQueryAllowedCharacterSet]];
 
 NSString * const DTDiscogsErrorDomain = @"DTDiscogs";
-
-@interface DTDiscogs () // private
-
-@property (nonatomic, strong) NSURLSession *session;
-
-@end
 
 @implementation DTDiscogs
 {
@@ -29,46 +22,41 @@ NSString * const DTDiscogsErrorDomain = @"DTDiscogs";
 }
 
 - (instancetype)initWithSessionConfiguration:
-                              (NSURLSessionConfiguration *)configuration
-{
+                            (NSURLSessionConfiguration *)configuration {
    self = [super init];
    
-   if (self)
-   {
+   if (self) {
       _configuration = configuration;
    }
    
    return self;
 }
 
-- (instancetype)init
-{
+// designated initializer
+- (instancetype)init {
    // use ephemeral config, we need no caching
    NSURLSessionConfiguration *config =
               [NSURLSessionConfiguration ephemeralSessionConfiguration];
    return [self initWithSessionConfiguration:config];
 }
 
-// turns the API_ENDPOINT into NSURL
-- (NSURL *)_endpointURL
-{
-   return [NSURL URLWithString:API_ENDPOINT];
-}
-
 // constructs the path for a method call
 - (NSURL *)_methodURLForPath:(NSString *)path
-						parameters:(NSDictionary *)parameters
-{
+						parameters:(NSDictionary *)parameters {
+   // turns the API_ENDPOINT into NSURL
+   NSURL *endpointURL = [NSURL URLWithString:API_ENDPOINT];
+   
 	if ([parameters count])
 	{
 		// sort keys to get same order every time
-		NSArray *sortedKeys = [[parameters allKeys] sortedArrayUsingSelector:@selector(compare:)];
+		NSArray *sortedKeys =
+                  [[parameters allKeys]
+                   sortedArrayUsingSelector:@selector(compare:)];
 		
 		// construct query string
 		NSMutableArray *tmpArray = [NSMutableArray array];
 	
-		for (NSString *key in sortedKeys)
-		{
+		for (NSString *key in sortedKeys) {
 			NSString *value = parameters[key];
 			
 			// URL-encode
@@ -76,27 +64,26 @@ NSString * const DTDiscogsErrorDomain = @"DTDiscogs";
 			NSString *encValue = URLENC(value);
 			
 			// combine into pairs
-			NSString *tmpStr = [NSString stringWithFormat:@"%@=%@", encKey, encValue];
+			NSString *tmpStr = [NSString stringWithFormat:@"%@=%@",
+                             encKey, encValue];
 			[tmpArray addObject:tmpStr];
-			
 		}
 		
 		// append query to path
-		path = [path stringByAppendingFormat:@"?%@", [tmpArray componentsJoinedByString:@"&"]];
+		path = [path stringByAppendingFormat:@"?%@",
+              [tmpArray componentsJoinedByString:@"&"]];
 	}
 	
    return [NSURL URLWithString:path
-                 relativeToURL:[self _endpointURL]];
+                 relativeToURL:endpointURL];
 }
 
 // construct a suitable error
 - (NSError *)_errorWithCode:(NSUInteger)code
-                          message:(NSString *)message
-{
+                          message:(NSString *)message {
    NSDictionary *userInfo;
    
-   if (message)
-   {
+   if (message) {
       userInfo = @{NSLocalizedDescriptionKey : message};
    }
    
@@ -113,18 +100,16 @@ NSString * const DTDiscogsErrorDomain = @"DTDiscogs";
    NSURL *methodURL = [self _methodURLForPath:path parameters:parameters];
    NSURLRequest *request = [NSURLRequest requestWithURL:methodURL];
    
-   NSURLSessionDataTask *task = [self.session
+   NSURLSessionDataTask *task = [[self session]
                                  dataTaskWithRequest:request
                                  completionHandler:^(NSData *data,
                                                 NSURLResponse *response,
-                                                     NSError *error)
-   {
+                                                     NSError *error) {
       NSError *retError = error;
       id result = nil;
       
-      // check for transport error, e.g. no network
-      if (retError)
-      {
+      // check for transport error, e.g. no network connection
+      if (retError) {
          
          completion(nil, retError);
          return;
@@ -134,8 +119,7 @@ NSString * const DTDiscogsErrorDomain = @"DTDiscogs";
       NSString *calledHost = [methodURL host];
       NSString *responseHost = [response.URL host];
       
-      if (![responseHost isEqualToString:calledHost])
-      {
+      if (![responseHost isEqualToString:calledHost]) {
          NSString *msg = [NSString stringWithFormat:
                           @"Expected result host to be '%@' but was '%@'",
                           calledHost, responseHost];
@@ -154,44 +138,33 @@ NSString * const DTDiscogsErrorDomain = @"DTDiscogs";
       NSLog(@"output at %@", fileInDocuments);
       */
       // needs to be a HTTP response to get the content type and status
-      if ([response isKindOfClass:[NSHTTPURLResponse class]])
-      {
-         // check for protocol error
-         NSHTTPURLResponse *httpResp = (NSHTTPURLResponse *)response;
-         NSDictionary *headers = [httpResp allHeaderFields];
-         NSString *contentType = headers[@"Content-Type"];
-         
-         if ([contentType isEqualToString:@"application/json"])
-         {
-            // parse either way, also API errors return a JSON response
-            result = [NSJSONSerialization JSONObjectWithData:data
-                                                     options:0
-                                                       error:&retError];
-         }
-         else
-         {
-            NSString *msg = [NSString stringWithFormat:
-                             @"Incorrect response with type '%@'",
-                             contentType];
-            retError = [self _errorWithCode:999 message:msg];
-         }
-         
-         if (httpResp.statusCode >= 400)
-         {
-            NSString *message = result[@"message"];
-            
-            retError = [self _errorWithCode:httpResp.statusCode
-                                    message:message];
-            
-            // wipe result, we have the message already in NSError
-            result = nil;
-         }
-         
-      }
-      else
-      {
+      if (![response isKindOfClass:[NSHTTPURLResponse class]]) {
          NSString *msg = @"Response is not an NSHTTPURLResponse";
          retError = [self _errorWithCode:999 message:msg];
+         completion(nil, retError);
+         return;
+      }
+      
+      // check for protocol error
+      NSHTTPURLResponse *httpResp = (NSHTTPURLResponse *)response;
+      NSDictionary *headers = [httpResp allHeaderFields];
+      NSString *contentType = headers[@"Content-Type"];
+      
+      if ([contentType isEqualToString:@"application/json"]) {
+         // parse either way, also API errors return a JSON response
+         result = [NSJSONSerialization JSONObjectWithData:data
+                                                  options:0
+                                                    error:&retError];
+      }
+      
+      if (httpResp.statusCode >= 400) {
+         NSString *message = result[@"message"];
+         
+         retError = [self _errorWithCode:httpResp.statusCode
+                                 message:message];
+         
+         // wipe result, we have the message already in NSError
+         result = nil;
       }
       
       completion(result, retError);
@@ -202,15 +175,13 @@ NSString * const DTDiscogsErrorDomain = @"DTDiscogs";
 }
 
 - (void)searchForGTIN:(NSString *)gtin
-           completion:(DTDiscogsCompletion)completion
-{
+           completion:(DTDiscogsCompletion)completion {
 	// assert that all parameters are not nil
    NSParameterAssert(gtin);
    NSParameterAssert(completion);
    
    // convert EAN-13 to UPC if leading 0
-   if ([gtin length]==13 && [gtin hasPrefix:@"0"])
-   {
+   if ([gtin length]==13 && [gtin hasPrefix:@"0"]) {
       gtin = [gtin substringFromIndex:1];
    }
    
@@ -225,10 +196,8 @@ NSString * const DTDiscogsErrorDomain = @"DTDiscogs";
 
 
 // lazy initializer for URL session
-- (NSURLSession *)session
-{
-   if (!_session)
-   {
+- (NSURLSession *)session {
+   if (!_session) {
       _session = [NSURLSession sessionWithConfiguration:_configuration];
    }
    
