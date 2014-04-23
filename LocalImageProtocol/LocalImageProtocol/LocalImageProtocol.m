dca7420873966c8c6411f2a8c92a5d086b5a7ab8
Oliver Drobnik <oliver@cocoanetics.com>
2014-04-23 16:41:55 +0200

    Cleanup for book

--- LocalImageProtocol/LocalImageProtocol/LocalImageProtocol.m
+++ LocalImageProtocol/LocalImageProtocol/LocalImageProtocol.m
@@ -23,47 +23,51 @@
    return request;
 }
 
-- (void)startLoading
-{
-   NSString *fileName = [self.request.URL resourceSpecifier];
+- (void)_sendImageAtPath:(NSString *)imagePath {
+   // create HTTP response
+   NSHTTPURLResponse *response = [[NSHTTPURLResponse alloc]
+                                  initWithURL:self.request.URL
+                                  statusCode:200
+                                  HTTPVersion:@"1.1"
+                                  headerFields:nil];
    
+   // send response to client
+   [self.client URLProtocol:self
+         didReceiveResponse:response
+         cacheStoragePolicy:NSURLCacheStorageNotAllowed];
+   
+   // get file data and send it to client
+   NSData *data = [NSData dataWithContentsOfFile:imagePath];
+   
+   if ([data length]) {
+      [self.client URLProtocol:self didLoadData:data];
+   }
+   
+   // tell client that we're done
+   [self.client URLProtocolDidFinishLoading:self];
+}
+
+- (void)_sendError {
+   // send error to client
+   NSDictionary *info = @{NSLocalizedDescriptionKey:
+                             @"Cannot find file in app bundle"};
+   NSError *error = [NSError
+                     errorWithDomain:NSStringFromClass([self class])
+                     code:999 userInfo:info];
+   
+   [self.client URLProtocol:self didFailWithError:error];
+}
+
+- (void)startLoading {
+   NSString *fileName = [self.request.URL resourceSpecifier];
    NSBundle *bundle = [NSBundle mainBundle];
    NSString *imagePath = [bundle pathForResource:fileName ofType:nil];
    
    if (imagePath) {
-      // create HTTP response
-      NSHTTPURLResponse *response = [[NSHTTPURLResponse alloc]
-                                     initWithURL:self.request.URL
-                                     statusCode:200
-                                     HTTPVersion:@"1.1"
-                                     headerFields:nil];
-      
-      // send response to client
-      [self.client URLProtocol:self
-            didReceiveResponse:response
-            cacheStoragePolicy:NSURLCacheStorageNotAllowed];
-      
-      // get file data and send it to client
-      NSData *data = [NSData dataWithContentsOfFile:imagePath];
-      
-      if ([data length]) {
-         [self.client URLProtocol:self didLoadData:data];
-      }
-      
-      // tell client that we're done
-      [self.client URLProtocolDidFinishLoading:self];
-      
+      [self _sendImageAtPath:imagePath];
    }
    else {
-      // send error to client
-      NSDictionary *info = @{NSLocalizedDescriptionKey:
-                                @"Cannot find file in app bundle"};
-      NSError *error = [NSError
-                        errorWithDomain:NSStringFromClass([self class])
-                        code:999 userInfo:info];
-      
-      [self.client URLProtocol:self
-              didFailWithError:error];
+      [self _sendError];
    }
 }
 
