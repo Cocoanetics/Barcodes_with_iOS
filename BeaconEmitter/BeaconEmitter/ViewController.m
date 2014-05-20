6514e3dd7bfe30d2737efe0dc311a9d058552dfc
Oliver Drobnik <oliver@cocoanetics.com>
2014-05-20 14:17:29 +0200

    Added BeaconEmitter

---
+++ BeaconEmitter/BeaconEmitter/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BeaconEmitter
+//
+//  Created by Oliver Drobnik on 20.05.14.
+//  Copyright (c) 2014 Cocoanetics. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
