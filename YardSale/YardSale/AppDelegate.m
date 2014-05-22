6b76744c6869759c7b14a50fa815b8b1ce42fe51
Oliver Drobnik <oliver@cocoanetics.com>
2014-05-22 10:42:40 +0200

    formatting for book style

--- YardSale/YardSale/AppDelegate.m
+++ YardSale/YardSale/AppDelegate.m
@@ -23,13 +23,15 @@
    NSString *_lastNotifiedSaleID;
 }
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+- (BOOL)application:(UIApplication *)application
+             didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
    // create sale manager
    _saleManager = [YardSaleManager new];
    
    // pass it to root map view
-   MapViewController *vc = (MapViewController *)self.window.rootViewController;
+   MapViewController *vc =
+      (MapViewController *)self.window.rootViewController;
    NSAssert([vc isKindOfClass:[MapViewController class]],
             @"Root VC is not a MapViewController!");
    vc.yardSaleManager = _saleManager;
@@ -40,45 +42,30 @@
    return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application
-{
-   // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-   // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-   // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-   // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-   // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
-   // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-   
+   // update monitored regions when app becomes active
    if (_mostRecentLoc)
    {
       [self _updateMonitoredRegionsForLocation:_mostRecentLoc];
    }
 }
 
-- (void)applicationWillTerminate:(UIApplication *)application
-{
-   // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-}
-
 // received after the user reacts to the local notification action or if the app is in foreground during receipt of the notification
-- (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification
+- (void)application:(UIApplication *)application
+         didReceiveLocalNotification:(UILocalNotification *)notification
 {
-   SalePlacemark *salePlace = [self _salePlaceForIdentifier:notification.userInfo[@"SaleID"]];
-   NSString *msg = [NSString stringWithFormat:@"Welcome to %@", salePlace.title];
+   SalePlacemark *salePlace =
+      [self _salePlaceForIdentifier:notification.userInfo[@"SaleID"]];
+   NSString *msg = [NSString stringWithFormat:@"Welcome to %@",
+                    salePlace.title];
    
-   UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Glad to see you!" message:msg delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil];
+   UIAlertView *alert = [[UIAlertView alloc]
+                         initWithTitle:@"Glad to see you!"
+                         message:msg
+                         delegate:nil
+                         cancelButtonTitle:@"Ok"
+                         otherButtonTitles:nil];
    [alert show];
 }
 
@@ -86,33 +73,30 @@
 
 - (void)_enableLocationUpdatesIfAuthorized
 {
-   CLAuthorizationStatus authStatus = [CLLocationManager authorizationStatus];
+   CLAuthorizationStatus authStatus =
+      [CLLocationManager authorizationStatus];
    
    switch (authStatus)
    {
       case kCLAuthorizationStatusNotDetermined:
       case kCLAuthorizationStatusAuthorized:
       {
-         NSLog(@"authorized or not determined");
-         
          // initialize location manager
          if (!_locationMgr)
-        {
+         {
             _locationMgr = [[CLLocationManager alloc] init];
             _locationMgr.delegate = self;
-           
-//           [_locationMgr allowDeferredLocationUpdatesUntilTraveled:_locationMgr.maximumRegionMonitoringDistance/2.0 timeout:CLTimeIntervalMax];
-           
+            
+            // used for normal operation, only interested if there is a significant change in location
             [_locationMgr startMonitoringSignificantLocationChanges];
-           
-           
-           // used for testing because simulator does not send updates for significant location change monitoring
-           
-           /*
-           _locationMgr.distanceFilter = 1000;
-           _locationMgr.desiredAccuracy = kCLLocationAccuracyKilometer;
-           [_locationMgr startUpdatingLocation];
-            */
+            
+            // used for testing because simulator does not send updates for significant location change monitoring
+            
+            /*
+             _locationMgr.distanceFilter = 1000;
+             _locationMgr.desiredAccuracy = kCLLocationAccuracyKilometer;
+             [_locationMgr startUpdatingLocation];
+             */
          }
          break;
       }
@@ -120,8 +104,6 @@
       case kCLAuthorizationStatusDenied:
       case kCLAuthorizationStatusRestricted:
       {
-         NSLog(@"policy has restricted location updates or user denied it");
-         
          _locationMgr = nil;
          break;
       }
@@ -139,12 +121,11 @@
    }
    
    // get closest 10 yard sales
-   NSMutableArray *sales = [[_saleManager
-                         annotationsClosestToLocation:loc] mutableCopy];
+   NSArray *sales = [_saleManager annotationsClosestToLocation:loc];
    
    // IDs to monitor
    NSMutableArray *identsToMonitor =
-    [[sales valueForKeyPath:@"@unionOfObjects.identifier"] mutableCopy];
+   [[sales valueForKeyPath:@"@unionOfObjects.identifier"] mutableCopy];
    
    for (CLRegion *region in _locationMgr.monitoredRegions)
    {
@@ -165,7 +146,8 @@
    
    for (SalePlacemark *onePlace in sales)
    {
-      CLLocationDistance dist = [loc distanceFromLocation:onePlace.location];
+      CLLocationDistance dist =
+         [loc distanceFromLocation:onePlace.location];
       maxDistance = MAX(dist, maxDistance);
       
       if (![identsToMonitor containsObject:onePlace.identifier])
@@ -184,31 +166,34 @@
    
    // update deferred updates to half of max distance
    [_locationMgr
-       allowDeferredLocationUpdatesUntilTraveled:maxDistance/2.0
-                                         timeout:CLTimeIntervalMax];
-
+    allowDeferredLocationUpdatesUntilTraveled:maxDistance/2.0
+    timeout:CLTimeIntervalMax];
+   
    // requires slight delay, fails otherwise if a region was unmonitored
    dispatch_after(dispatch_time(DISPATCH_TIME_NOW,
                                 (int64_t)(0.2 * NSEC_PER_SEC)),
                   dispatch_get_main_queue(), ^{
-      for (CLRegion *oneRegion in _locationMgr.monitoredRegions)
-      {
-         [_locationMgr requestStateForRegion:oneRegion];
-      }
-   });
+                     for (CLRegion *oneRegion in
+                          _locationMgr.monitoredRegions)
+                     {
+                        [_locationMgr requestStateForRegion:oneRegion];
+                     }
+                  });
 }
 
-- (void)_sendLocalNoteAfterDuration:(NSTimeInterval)duration
-                            message:(NSString *)msg
-                          soundName:(NSString *)sound
-                           userInfo:(NSDictionary *)userInfo
+// sends a local notification for a Yard Sale Place
+- (void)_sendLocalNoteForSalePlace:(SalePlacemark *)place
+                     afterDuration:(NSTimeInterval)duration
 {
+   NSString *msg = [NSString stringWithFormat:@"%@ is closeby!",
+                    place.title];
+   
    UILocalNotification *note = [[UILocalNotification alloc] init];
    note.alertAction = @"Visit";
    note.alertBody = msg;
    note.fireDate = [[NSDate date] dateByAddingTimeInterval:duration];
-   note.soundName = sound;
-   note.userInfo = userInfo;
+   note.soundName = UILocalNotificationDefaultSoundName;
+   note.userInfo = @{@"SaleID": place.identifier};
    
    [[UIApplication sharedApplication] scheduleLocalNotification:note];
 }
@@ -216,9 +201,9 @@
 - (SalePlacemark *)_salePlaceForIdentifier:(NSString *)identifier
 {
    NSPredicate *predicate =
-      [NSPredicate predicateWithFormat:@"identifier == %@", identifier];
+   [NSPredicate predicateWithFormat:@"identifier == %@", identifier];
    NSArray *matches =
-     [[_saleManager annotations] filteredArrayUsingPredicate:predicate];
+   [[_saleManager annotations] filteredArrayUsingPredicate:predicate];
    return [matches firstObject];
 }
 
@@ -227,8 +212,6 @@
 - (void)locationManager:(CLLocationManager *)manager
      didUpdateLocations:(NSArray *)locations
 {
-   NSLog(@"%s", __PRETTY_FUNCTION__);
-   
    CLLocation *location = [locations lastObject];
    
    if (location.coordinate.longitude
@@ -266,13 +249,9 @@
          }
          
          SalePlacemark *salePlace =
-            [self _salePlaceForIdentifier:region.identifier];
-         NSString *msg = [NSString stringWithFormat:@"%@ is closeby!",
-                          salePlace.title];
-         NSDictionary *userInfo = @{@"SaleID": region.identifier};
-         [self _sendLocalNoteAfterDuration:5 message:msg
-                           soundName:UILocalNotificationDefaultSoundName
-                                  userInfo:userInfo];
+         [self _salePlaceForIdentifier:region.identifier];
+         [self _sendLocalNoteForSalePlace:salePlace
+                            afterDuration:0];
          
          _lastNotifiedSaleID = region.identifier;
          
@@ -292,7 +271,7 @@
 monitoringDidFailForRegion:(CLRegion *)region
               withError:(NSError *)error
 {
-   NSLog(@"%@", error);
+   NSLog(@"Monitoring failure: %@", [error localizedDescription]);
 }
 
 @end
