24fe4996f2a285f819bf8ce99dfda50b9d578827
Oliver Drobnik <oliver@cocoanetics.com>
2014-05-27 16:02:13 +0200

    Removed unnecessary import

--- YardSale/YardSale/SalePlace.h
+++ YardSale/YardSale/SalePlace.h
@@ -6,8 +6,6 @@
 //  Copyright (c) 2014 Cocoanetics. All rights reserved.
 //
 
-#import <MapKit/MapKit.h>
-
 @interface SalePlace : MKPlacemark
 
 - (instancetype)initWithDictionary:(NSDictionary *)dictionary;
