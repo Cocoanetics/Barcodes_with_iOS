4637e21c1c953c36d40b0a1598cd7f49e329a926
Oliver Drobnik <oliver@cocoanetics.com>
2014-05-28 17:11:58 +0200

    Formatting for book

--- YardSale/YardSale/MapViewController.m
+++ YardSale/YardSale/MapViewController.m
@@ -19,8 +19,7 @@
 @implementation MapViewController
 
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
    [super viewDidLoad];
    
    // get all annotations from manager
@@ -35,24 +34,27 @@
 
 #pragma mark - MKMapViewDelegate
 
-- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation {
-   
-   if ([annotation isKindOfClass:[MKUserLocation class]])
-   {
+- (MKAnnotationView *)mapView:(MKMapView *)mapView
+            viewForAnnotation:(id<MKAnnotation>)annotation {
+   if ([annotation isKindOfClass:[MKUserLocation class]]) {
       return nil;
    }
    
-   MKPinAnnotationView *pav = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:nil];
-   pav.canShowCallout = YES; // otherwise no callout is shown on selection
+   MKPinAnnotationView *pav =
+      [[MKPinAnnotationView alloc] initWithAnnotation:annotation
+                                      reuseIdentifier:nil];
+   // otherwise no callout is shown on selection
+   pav.canShowCallout = YES;
    
-   UIButton *detailButton = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
+   UIButton *detailButton =
+      [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
    pav.rightCalloutAccessoryView=detailButton;
-   pav.enabled = YES;
    return pav;
 }
 
-- (void)mapView:(MKMapView *)mapView annotationView:(MKAnnotationView *)view calloutAccessoryControlTapped:(UIControl *)control
-{
+- (void)mapView:(MKMapView *)mapView
+ annotationView:(MKAnnotationView *)view
+ calloutAccessoryControlTapped:(UIControl *)control {
 //   // hide the bubble
    [mapView deselectAnnotation:view.annotation animated:YES];
    
@@ -62,24 +64,20 @@
 
 #pragma mark - Navigation
 
-- (void)showInStoreUIForSalePlace:(SalePlace *)place
-{
+- (void)showInStoreUIForSalePlace:(SalePlace *)place {
    MKAnnotationView *view = [self.mapView viewForAnnotation:place];
    [self performSegueWithIdentifier:@"ShowSalePlace" sender:view];
 }
 
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-   if ([segue.identifier isEqualToString:@"ShowSalePlace"])
-   {
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+   if ([segue.identifier isEqualToString:@"ShowSalePlace"]) {
       UINavigationController *nav = [segue destinationViewController];
       InStoreViewController *vc = nav.viewControllers[0];
       vc.salePlace = [sender annotation];
    }
 }
 
-- (IBAction)unwindFromStoreDetail:(UIStoryboardSegue *)segue
-{
+- (IBAction)unwindFromStoreDetail:(UIStoryboardSegue *)segue {
    // dummy, nothing to do here
 }
 
