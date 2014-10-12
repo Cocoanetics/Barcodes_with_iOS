8811587bd2e96eff8ad8fd42d81a7aa45d7bde56
Geoff Breemer <gmbreemer@gmail.com>
2014-10-12 19:59:58 +1000

    Fixed outlets not linked to storyboard. Moved AVCaptureMetadataOutputObjectsDelegate fund to extension. Updated project structure.

---
+++ QRScanner/ObjC/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  QRScanner
+//
+//  Created by Oliver Drobnik on 10/12/13.
+//  Copyright (c) 2013 Oliver Drobnik. All rights reserved.
+//
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+// the application's main window
+@property (strong, nonatomic) UIWindow *window;
+
+@end
