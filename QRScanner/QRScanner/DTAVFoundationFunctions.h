9f2b0261421dcd7b0a6794a5e50264774c188d47
Geoff Breemer <gmbreemer@gmail.com>
2014-10-12 20:26:57 +1000

    Clean up

--- QRScanner/QRScanner/DTAVFoundationFunctions.h
+++
@@ -1,9 +0,0 @@
-// helper function to convert interface orienation to correct video capture orientation
-AVCaptureVideoOrientation
-   DTAVCaptureVideoOrientationForUIInterfaceOrientation(
-                           UIInterfaceOrientation interfaceOrientation);
-
-// creates a CGPath for the cornder of a barcode object
-CGPathRef DTAVMetadataMachineReadableCodeObjectCreatePathForCorners(
-                               AVCaptureVideoPreviewLayer *previewLayer,
-                    AVMetadataMachineReadableCodeObject *barcodeObject);
